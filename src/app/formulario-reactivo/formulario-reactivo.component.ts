fa7890b97fa06c76991c72e3d8cba53948c31847
Ariel Salcito <ariel_salcito@live.com>
2023-05-13 01:53:45 -0300

    Children

--- src/app/formulario-reactivo/formulario-reactivo.component.ts
+++ src/app/formulario-reactivo/formulario-reactivo.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-formulario-reactivo',
@@ -8,6 +8,29 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class FormularioReactivoComponent {
 
-  name = new FormControl('', Validators.required);
-  email = new FormControl('', [Validators.required, Validators.email]);
+  constructor(private fb: FormBuilder){
+
+  }
+
+  get name(){
+    return this.formUser.get('name') as FormControl;
+  }
+
+  get email(){
+    return this.formUser.get('email') as FormControl;
+  }
+
+  /* formUser = new FormGroup({
+    'name': new FormControl('', Validators.required),
+    'email': new FormControl('', [Validators.required, Validators.email])
+  }); */
+
+  formUser = this.fb.group({
+    'name': ['', Validators.required],
+    'email': ['', [Validators.required, Validators.email]]
+  });
+
+  procesar(){
+    console.log(this.formUser.value)
+  }
 }
